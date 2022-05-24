7e2ac0fb64da13528b619eb3d7398ffd04228e36
Thiago Cerqueira <thicerqs@gmail.com>
2022-05-24 18:00:18 -0300

    MainMenu funcional

--- Projeto-Granja/Assets/Scripts/MenuButtonSystem.cs
+++ Projeto-Granja/Assets/Scripts/MenuButtonSystem.cs
@@ -5,8 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtonSystem : MonoBehaviour
 {
+    public GameObject mainMenu;
+    public GameObject settingsMenu;
+    public GameObject creditsMenu;
+
     public void Play()
     {
         SceneManager.LoadScene("Play");
+    }
+
+    public void OpenSettings()
+    {
+        settingsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void CloseSettings()
+    {
+        settingsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void OpenCredits()
+    {
+        creditsMenu.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void CloseCredits()
+    {
+        creditsMenu.SetActive(false);
+        mainMenu.SetActive(true);
     }
 }
