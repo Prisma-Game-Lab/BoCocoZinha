fb7c8fad3f01212500bc3f745ad9bdd23c28a810
Thiago Cerqueira <thicerqs@gmail.com>
2022-06-05 09:57:34 -0300

    Hurtboxes pt1
    Criação dos scripts

--- Projeto-Granja/Assets/Scripts/Player/PlayerMovement.cs
+++ Projeto-Granja/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private float movX;
     private float movY;
+    private Vector2 movVector;
+    public int MoveSpeed = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +19,13 @@ public class PlayerMovement : MonoBehaviour
 
     void OnMove(InputValue mov)
     {
-        Vector2 movVector = mov.Get<Vector2>();
+        movVector = mov.Get<Vector2>();
         movX = movVector.x;
         movY = movVector.y;
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(new Vector2(5*movX, 5*movY));
+        rb.MovePosition(rb.position + movVector * MoveSpeed * Time.fixedDeltaTime);
     }
 }
