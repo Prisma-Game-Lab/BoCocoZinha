cfd3ab3a3c52d346ebc63ee8ea93ed584275180d
vitorsalek <vitorsalek@hotmail.com>
2022-06-22 16:13:59 -0300

    Sounds effects and ui fix

--- Projeto-Granja/Assets/Scripts/UI/MenuButtonSystem.cs
+++ Projeto-Granja/Assets/Scripts/UI/MenuButtonSystem.cs
@@ -8,38 +8,45 @@ public class MenuButtonSystem : MonoBehaviour
     public GameObject mainMenu;
     public GameObject settingsMenu;
     public GameObject creditsMenu;
+    public AudioManager audioManager;
 
     public void Play()
     {
+        audioManager.Play("button_in");
         SceneManager.LoadScene("Playtest Scene");
     }
 
     public void OpenSettings()
     {
+        audioManager.Play("button_in");
         settingsMenu.SetActive(true);
         mainMenu.SetActive(false);
     }
 
     public void CloseSettings()
     {
+        audioManager.Play("button_out");
         settingsMenu.SetActive(false);
         mainMenu.SetActive(true);
     }
 
     public void OpenCredits()
     {
+        audioManager.Play("button_in");
         creditsMenu.SetActive(true);
         mainMenu.SetActive(false);
     }
 
     public void CloseCredits()
     {
+        audioManager.Play("button_out");
         creditsMenu.SetActive(false);
         mainMenu.SetActive(true);
     }
 
     public void QuitGame()
     {
+        audioManager.Play("button_out");
         Application.Quit();
     }
 }
