dbaedab3324e9309ef1b597a814914ec328d5011
vitorsalek <vitorsalek@hotmail.com>
2022-07-08 16:25:55 -0300

    Audio manager persistindo entre cenas

--- Projeto-Granja/Assets/Scripts/Sound/AudioManager.cs
+++ Projeto-Granja/Assets/Scripts/Sound/AudioManager.cs
@@ -8,9 +8,19 @@ public class AudioManager : MonoBehaviour
 	//public AudioMixerGroup mixerGroup;
 
 	public Sound[] sounds;
+	public static AudioManager instance;
 
 	void Awake()
 	{
+		if (instance == null)
+			instance = this;
+		else
+		{
+			Destroy(gameObject);
+			return;
+		}
+		DontDestroyOnLoad(gameObject);
+
 		foreach (Sound s in sounds)
 		{
 			s.source = gameObject.AddComponent<AudioSource>();
