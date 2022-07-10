481a3892dbdb3eb7352f17bb51732919ed3cc040
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2022-07-09 23:55:51 -0300

    Settings menu

--- Projeto-Granja/Assets/Scripts/Sound/SoundVolume.cs
+++ Projeto-Granja/Assets/Scripts/Sound/SoundVolume.cs
@@ -14,22 +14,51 @@ public class SoundVolume : MonoBehaviour
     private void Start()
     {
         if (PlayerPrefs.HasKey("musicVol"))
+        {
             musicVolume.value = PlayerPrefs.GetFloat("musicVol");
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("musicVol", 0.5f);
+            musicVolume.value = PlayerPrefs.GetFloat("musicVol");
+            mixer.SetFloat("musicVol", Mathf.Log10(0.9999f - musicVolume.value) * 25);
+        }
+
         if (PlayerPrefs.HasKey("overallVol"))
+        {
             overallVolume.value = PlayerPrefs.GetFloat("overallVol");
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("overallVol", 0.5f);
+            overallVolume.value = PlayerPrefs.GetFloat("overallVol");
+            mixer.SetFloat("overallVol", Mathf.Log10(0.9999f - overallVolume.value) * 25);
+        }
+
         if (PlayerPrefs.HasKey("sfxVol"))
+        {
             sfxVolume.value = PlayerPrefs.GetFloat("sfxVol");
+        }
+        else
+        {
+            PlayerPrefs.SetFloat("sfxVol", 0.5f);
+            sfxVolume.value = PlayerPrefs.GetFloat("sfxVol");
+            mixer.SetFloat("sfxVol", Mathf.Log10(0.9999f - sfxVolume.value) * 25);
+        }
     }
+
     public void SetMusictrackVolume()
     {
         PlayerPrefs.SetFloat("musicVol", musicVolume.value);
         mixer.SetFloat("musicVol", Mathf.Log10(0.9999f - musicVolume.value) * 25);
     }
+    
     public void SetoveralltrackVolume()
     {
         PlayerPrefs.SetFloat("overallVol", overallVolume.value);
         mixer.SetFloat("overallVol", Mathf.Log10(0.9999f - overallVolume.value) * 25);
     }
+
     public void SetsfxtrackVolume()
     {
         PlayerPrefs.SetFloat("sfxVol", sfxVolume.value);
