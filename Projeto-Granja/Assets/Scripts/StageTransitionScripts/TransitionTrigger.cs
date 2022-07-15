31a3b4fcfa54ebda0d09b736c60e850a743014bf
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2022-07-14 21:44:30 -0300

    Scene Transition

---
+++ Projeto-Granja/Assets/Scripts/StageTransitionScripts/TransitionTrigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionTrigger : MonoBehaviour
+{
+    public int sceneIndex;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            ChangeScene();
+        }    
+    }
+}
