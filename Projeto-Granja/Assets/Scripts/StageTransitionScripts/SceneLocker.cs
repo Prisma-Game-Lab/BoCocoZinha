4aa04dd4c6f98a0021497797c0509de7a7478985
Rasenganlipe <felipe.antelo.machado@gmail.com>
2022-07-28 00:03:45 -0300

    Fechamento das salas
    Adicionar o fechamento das salas de inimigo

---
+++ Projeto-Granja/Assets/Scripts/StageTransitionScripts/SceneLocker.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SceneLocker : MonoBehaviour
+{
+    public List<GameObject> enemies;
+    private bool locked;
+    public List<GameObject> doors;
+    // Start is called before the first frame update
+    void Start()
+    {
+        locked = false;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (locked)
+        {
+            CheckEnemies();
+        }
+    }
+    
+    private void CheckEnemies()
+    {
+        foreach (GameObject enemy in enemies)
+        {
+            if (enemy != null)
+            {
+                Debug.Log("close door");
+                return;
+            }
+
+        }
+        OpenDoor();
+        Debug.Log("open door");
+        return ;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            CloseDoor();
+        }
+    }
+    public void CloseDoor()
+    {
+        locked = true;
+        for (int i=0;i<doors.Count;i++) //(initial valor;condition;adition per frame)
+        {
+            doors[i].gameObject.SetActive(true);
+        }
+    }
+    public void OpenDoor()
+    {
+        locked = false;
+        for (int i = 0; i < doors.Count; i++)
+        {
+            doors[i].gameObject.SetActive(false);
+        }
+    }
+}
+
