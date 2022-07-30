5d9cd36216f7961aed5d5f25817df11d410692bd
vitorsalek <vitorsalek@hotmail.com>
2022-07-30 00:38:53 -0300

    sounds volumes tweaks

--- Projeto-Granja/Assets/Scripts/Sound/PlaySceneSounds.cs
+++ Projeto-Granja/Assets/Scripts/Sound/PlaySceneSounds.cs
@@ -6,15 +6,17 @@ public class PlaySceneSounds : MonoBehaviour
 {
     public string[] musicName;
     public bool stopOtherMusics;
+    public bool stopEffects;
     // Start is called before the first frame update
     void Start()
     {
         if (stopOtherMusics)
             AudioManager.instance.StopAllMusicSounds();
+        if(stopEffects)
+            AudioManager.instance.StopAllEffectsSounds();
         foreach (string item in musicName)
         {
             AudioManager.instance.Play(item);
         }
-        
     }
 }
